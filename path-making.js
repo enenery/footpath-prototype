79acaa90688a1ddf1ba40140ea735e4a01d116d4
enenery <erii.sugimoto@stu.bmcc.cuny.edu>
2017-08-17 10:48:32 -0400

    Make the depth of each closed door to be 3px

--- path-making.js
+++ path-making.js
@@ -12,14 +12,14 @@ let PATH1 = new Path(); //user's expected path
 
 //make rectangle map with doors - each door is 50 px in x (width) and 7 px in y (depth), each door is seperated by 100 px 
 POLYGON1.pathPoints = [ new Point(50, 100), new Point(100, 100),
-						new Point(100, 93), new Point(150, 93), new Point(150, 100), new Point(250, 100),
-						new Point(250, 93), new Point(300, 93), new Point(300, 100), new Point(400, 100),
-						new Point(400, 93), new Point(450, 93), new Point(450, 100), new Point(550, 100),
-						new Point(550, 93), new Point(600, 93), new Point(600, 100), new Point(700, 100),
-						new Point(700, 93), new Point(750, 93), new Point(750, 100), new Point(750, 200),
-						new Point(757, 200), new Point(757, 250), new Point(750, 250), new Point(750, 350),
-						new Point(757, 350), new Point(757, 400), new Point(750, 400), new Point(750, 500),
-						new Point(757, 500), new Point(757, 550), new Point(50, 550),
+						new Point(100, 97), new Point(150, 97), new Point(150, 100), new Point(250, 100),
+						new Point(250, 97), new Point(300, 97), new Point(300, 100), new Point(400, 100),
+						new Point(400, 97), new Point(450, 97), new Point(450, 100), new Point(550, 100),
+						new Point(550, 97), new Point(600, 97), new Point(600, 100), new Point(700, 100),
+						new Point(700, 97), new Point(750, 97), new Point(750, 100), new Point(750, 200),
+						new Point(753, 200), new Point(753, 250), new Point(750, 250), new Point(750, 350),
+						new Point(753, 350), new Point(753, 400), new Point(750, 400), new Point(750, 500),
+						new Point(753, 500), new Point(753, 550), new Point(50, 550),
 						];
 
 //make basic rectangle map with NO doors
