dceb379df364f443f00084e2a4d5376773ca23f8
enenery <erii.sugimoto@stu.bmcc.cuny.edu>
2017-08-18 17:09:09 -0400

    Add a random map and its user's expected path

--- path-making.js
+++ path-making.js
@@ -3,6 +3,7 @@ const endOfX = 735; //user's expected path's end location in x
 const endOfY = 535; //user's expected path's end location in y
 const startOfX = 125; //user's starting location in x
 const startOfY = 115; //user's starting location in y
+const userOneStepSize = 15; //user's one step size 15 px = 30 inches = ~70 cm
 
 let x = startOfX;
 let y = startOfY;
@@ -10,6 +11,7 @@ let y = startOfY;
 let POLYGON1 = new Path(); //map
 let PATH1 = new Path(); //user's expected path
 
+/*
 //make rectangle map with closed doors - each door is 50 px in x (width) and 3 px in y (depth), each door is seperated by 100 px 
 POLYGON1.pathPoints = [ new Point(50, 100), new Point(100, 100),
 						new Point(100, 97), new Point(150, 97), new Point(150, 100), new Point(250, 100),
@@ -21,6 +23,7 @@ POLYGON1.pathPoints = [ new Point(50, 100), new Point(100, 100),
 						new Point(753, 350), new Point(753, 400), new Point(750, 400), new Point(750, 500),
 						new Point(753, 500), new Point(753, 550), new Point(50, 550),
 						];
+*/
 
 /*
 //make rectangle map with open doors - each door is 50 px in x (width) and to simulate open doors, 
@@ -36,29 +39,69 @@ POLYGON1.pathPoints = [ new Point(50, 100), new Point(100, 100),
 						new Point(790, 500), new Point(790, 550), new Point(50, 550)
 						];				
 */
+
 //make basic rectangle map with NO doors
 //POLYGON1.pathPoints = [ new Point(50, 100), new Point(750, 100), new Point(750, 550), new Point(50, 550)];
 
+//make a random map
+POLYGON1.pathPoints = [new Point(50, 100), new Point(100, 100), new Point(100, 97), new Point(130, 97), new Point(130, 100),
+						new Point(200, 100), new Point(200, 60), new Point(230, 60), new Point(230, 100), new Point(250, 100),
+						new Point(250, 97), new Point(280, 97), new Point(280, 100), new Point(500, 100), new Point(500, 60),
+						new Point(550, 60), new Point(550, 130), new Point(553, 130), new Point(553, 150), new Point(550, 150),
+						new Point(550, 150), new Point(550, 200), new Point(553, 200), new Point(553, 220), new Point(550, 220),
+						new Point(550, 240), new Point(600, 240), new Point(600, 270), new Point(550, 270), new Point(550, 400), 
+						new Point(600, 400), new Point(600, 350), new Point(630, 350), new Point(630, 400),
+						new Point(700, 400), new Point(700, 430), new Point(703, 430), new Point(703, 480), new Point(700, 480),
+						new Point(700, 500), new Point(703, 500), new Point(703, 550), new Point(700, 550),
+						new Point(700, 580), new Point(400, 580), new Point(400, 300), new Point(50, 300)];
 
-	//make user's expected path involving a turn
+/*
+//make user's expected path involving a turn
+while(x <= endOfX){
+	PATH1.addPoint(new Point(x, startOfY));
+	x += userOneStepSize;
+}
 
-	while(x <= endOfX){
-		PATH1.addPoint(new Point(x, startOfY));
-		x += 15;
-	}
-
-	while(y <= endOfY){
-		PATH1.addPoint(new Point(endOfX, y));
-		y += 15;
-	}
+while(y <= endOfY){
+	PATH1.addPoint(new Point(endOfX, y));
+	y += userOneStepSize;
+}
+*/
 
 /*
 	//make user's expected path NOT involving a turn
 	while(x <= endOfX){
 		PATH1.addPoint(new Point(x, startOfY));
-		x += 15;
+		x += userOneStepSize;
 	}
 */
+
+//make user's path for the random map
+while(x <= 535){
+	PATH1.addPoint(new Point(x, startOfY));
+	x += userOneStepSize;
+}
+
+while(y <= 415){
+	PATH1.addPoint(new Point(535, y));
+	y += userOneStepSize;
+}
+
+while(x <= 685){
+	PATH1.addPoint(new Point(x, 415));
+	x += userOneStepSize;
+}
+
+y = 415;
+while(y <= 530){
+	PATH1.addPoint(new Point(685, y));
+	y += userOneStepSize;
+}
+
+
+
+
+
 
 //fake user points for testing the points-csv feature
 let userPoints = [
@@ -241,7 +284,6 @@ function convertArrayOfObjectsToCSV(args) {
         	result += lineDelimiter;
         }
         
-
         return result;
     }
 
